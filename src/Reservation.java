05e277ba600db9667a789a844f0f753398da0a10
Sanalouafi <sanalouafi2003@gmail.com>
2024-09-02 14:17:35 +0100

    add new method --createReservation--

--- src/Reservation.java
+++ src/Reservation.java
@@ -9,6 +9,7 @@ public class Reservation {
     private Date date;
     private  static  List<Reservation> reservations=new ArrayList();
     public Reservation(Room room, Client client, Date date) {
+        this.id=counter++;
         this.room=room;
         this.client=client;
         this.date=date;
@@ -40,6 +41,21 @@ public class Reservation {
     }
     @Override
     public String toString() {
-        return "";
+        return "the reservation is done \n"+room+" for the client "+client;
     }
+    public static List<Reservation> getReservations() {
+        return  reservations;
+    }
+    public static void createReservation(Room room, Client client, Date date) {
+        for(Reservation reservation:reservations) {
+            if(reservation.getRoom().getNumber()==room.getNumber()&&reservation.getDate().equals(date)) {
+                System.out.println("Room " + room.getNumber() + " is not available on " + date);
+                return;
+            }
+        }
+        Reservation newRes=new Reservation(room,client,date);
+        reservations.add(newRes);
+        System.out.println("New Reservation created"+newRes);
+    }
+
 }
