2a7eccf19aab8313cade577fe0c37e7790a36cce
Sanalouafi <sanalouafi2003@gmail.com>
2024-09-02 11:00:46 +0100

    add getters and setters to the reservation class

--- src/Reservation.java
+++ src/Reservation.java
@@ -7,4 +7,39 @@ public class Reservation {
     private Room room;
     private Client client;
     private Date date;
+    private  static  List<Reservation> reservations=new ArrayList();
+    public Reservation(Room room, Client client, Date date) {
+        this.room=room;
+        this.client=client;
+        this.date=date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+    public void setClient(Client client) {
+        this.client=client;
+    }
+    public Room getRoom() {
+        return room;
+    }
+    public void setRoom(Room room) {
+        this.room=room;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date=date;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id=id;
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
 }
